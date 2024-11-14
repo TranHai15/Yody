2fce462020cda4b930a80389dea2429cb94a1d42
TranHai <hairobet15092005@gmail.com>
2024-11-14 15:58:46 +0700

    admin lam chuc nang

---
+++ Frontend/component/admin/home.php
@@ -0,0 +1,4 @@
+<?php
+
+
+echo "Hoame admin ";
---
+++ Frontend/component/admin/order.php
@@ -0,0 +1,5 @@
+<main id="main" class="main">
+    <div class="container mt-5">
+        <h2 class="text-center mb-4">Danh Sách order</h2>
+    </div>
+</main>
